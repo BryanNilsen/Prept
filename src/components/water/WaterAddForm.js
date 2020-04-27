35440849a9785a8ba189179e4b9084baa46aa43e
BryanNilsen <bryannilsen@gmail.com>
2020-04-27 11:59:05 -0500

    added waterform component to dynamically render based on add or edit

--- src/components/water/WaterAddForm.js
+++ src/components/water/WaterAddForm.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import APIManager from "../../modules/APIManager";
+import Calculations from "../../modules/Calculations";
 import water_icon from "../../images/water.png";
+import WaterForm from "./WaterForm";
 
 const WaterAddForm = (props) => {
+  const user = props.user;
   const [waterItem, setwaterItem] = useState({
     userId: 0,
     name: "",
-    qty: "",
+    qty: 0,
     oz: 0,
-    container: 0,
+    container: "",
   });
 
+  // prevent user from entering form multiple times by using this to disable buttons
   const [isLoading, setIsLoading] = useState(false);
 
   const handleFieldChange = (evt) => {
@@ -24,10 +28,16 @@ const WaterAddForm = (props) => {
     const newWaterItem = { ...waterItem };
     newWaterItem.userId = parseInt(sessionStorage.getItem("userId"));
 
+    // make sure user doesn't put in strings for numbers
+    if (!Number(newWaterItem.qty) || !Number(newWaterItem.oz)) {
+      alert("Please Enter a Number for Quantity and Ounces");
+      return;
+    }
+
     if (
       newWaterItem.name === "" ||
-      newWaterItem.qty === "" ||
-      newWaterItem.oz === "" ||
+      newWaterItem.qty === 0 ||
+      newWaterItem.oz === 0 ||
       newWaterItem.container === ""
     ) {
       window.alert("Please complete all fields");
@@ -43,69 +53,24 @@ const WaterAddForm = (props) => {
     <>
       <div className="main_content">
         <div className="main_content_header">
-          <div className="imgwrap">
-            <img src={water_icon} alt="water" className="main_header_img" />
+          <div className="header_lft">
+            <div className="imgwrap">
+              <img src={water_icon} alt="water" className="main_header_img" />
+            </div>
+            <h1>Water</h1>
           </div>
-          <h1>Water</h1>
+          <div className="header_details_rt">
+            <h1>{Calculations.calculateDaysOfWaterPerHousehold(user)}</h1>
+            <h4>days</h4>
+          </div>
         </div>
-        <div className="login_form_container">
-          <form className="login_form">
-            <h2>Add Water Item</h2>
-            <div className="form">
-              <label htmlFor="name">Name:</label>
-              <input
-                className="form_inputs"
-                type="text"
-                required
-                onChange={handleFieldChange}
-                id="name"
-                placeholder="Name"
-              />
-            </div>
-            <div className="form">
-              <label htmlFor="qty">Quantity:</label>
-              <input
-                className="form_inputs"
-                type="text"
-                required
-                onChange={handleFieldChange}
-                id="qty"
-                placeholder="quantity"
-              />
-            </div>
-            <div className="form">
-              <label htmlFor="oz">Ounces:</label>
-              <input
-                className="form_inputs"
-                type="text"
-                required
-                onChange={handleFieldChange}
-                id="oz"
-                placeholder="ounces"
-              />
-            </div>
-            <div className="form">
-              <label htmlFor="container">Container Type:</label>
-              <input
-                className="form_inputs"
-                type="text"
-                required
-                onChange={handleFieldChange}
-                id="container"
-                placeholder="bottle, can, etc."
-              />
-            </div>
-
-            <button
-              type="button"
-              className="btn-pink"
-              disabled={isLoading}
-              onClick={constructNewWaterItem}
-            >
-              Submit
-            </button>
-          </form>
-        </div>
+        <WaterForm
+          water={waterItem}
+          handleFieldChange={handleFieldChange}
+          isLoading={isLoading}
+          editWater={constructNewWaterItem}
+          isEdit={false}
+        />
       </div>
     </>
   );
--- src/components/water/WaterEditForm.js
+++ src/components/water/WaterEditForm.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import APIManager from "../../modules/APIManager";
 import Calculations from "../../modules/Calculations";
 import water_icon from "../../images/water.png";
+import WaterForm from "./WaterForm";
 
 const WaterEditForm = (props) => {
   const user = props.user;
@@ -63,68 +64,14 @@ const WaterEditForm = (props) => {
             <h4>days</h4>
           </div>
         </div>
-        <div className="login_form_container">
-          <form className="login_form">
-            <h2>Edit Water Item</h2>
-            <div className="form">
-              <label htmlFor="name">Name:</label>
-              <input
-                className="form_inputs"
-                type="text"
-                required
-                onChange={handleFieldChange}
-                id="name"
-                placeholder="Name"
-                value={water.name}
-              />
-            </div>
-            <div className="form">
-              <label htmlFor="qty">Quantity:</label>
-              <input
-                className="form_inputs"
-                type="text"
-                required
-                onChange={handleFieldChange}
-                id="qty"
-                placeholder="quantity"
-                value={water.qty}
-              />
-            </div>
-            <div className="form">
-              <label htmlFor="oz">Ounces:</label>
-              <input
-                className="form_inputs"
-                type="text"
-                required
-                onChange={handleFieldChange}
-                id="oz"
-                placeholder="ounces"
-                value={water.oz}
-              />
-            </div>
-            <div className="form">
-              <label htmlFor="container">Container Type:</label>
-              <input
-                className="form_inputs"
-                type="text"
-                required
-                onChange={handleFieldChange}
-                id="container"
-                placeholder="bottle, can, etc."
-                value={water.container}
-              />
-            </div>
 
-            <button
-              type="button"
-              className="btn-pink"
-              disabled={isLoading}
-              onClick={editWater}
-            >
-              Submit
-            </button>
-          </form>
-        </div>
+        <WaterForm
+          water={water}
+          handleFieldChange={handleFieldChange}
+          isLoading={isLoading}
+          editWater={editWater}
+          isEdit={true}
+        />
       </div>
     </>
   );
